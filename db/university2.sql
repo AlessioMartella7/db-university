95962e8ff650e0758f734be9b8ff0a31053873ae
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-17 15:15:33 +0200

    Contare gli insegnanti che hanno l'ufficio nello stesso edificio

--- db/university2.sql
+++ db/university2.sql
@@ -8,6 +8,12 @@ FROM `students`
 GROUP BY `anno_di_iscrizione` ;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) AS `numero_professori`, `office_address` AS `edificio` 
+FROM `teachers` 
+GROUP BY `edificio`;
+
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
